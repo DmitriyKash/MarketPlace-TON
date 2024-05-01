01a29b284e54fbdeec51bea05c0d19da56e8ea97
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-01 19:13:59 +0300

    Ваше сообщение о коммите

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react';
+
+function Login() {
+    const { connect, disconnect, wallet } = useTonConnectUI();
+
+    const handleLogin = async () => {
+        try {
+            await connect();
+            if (wallet) {
+                registerUser(wallet.address);
+            }
+        } catch (error) {
+            console.error('Login failed:', error);
+        }
+    };
+
+    const registerUser = (address) => {
+        fetch('http://localhost:3000/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ userAddress: address })
+        })
+        .then(response => response.json())
+        .then(data => console.log('Success:', data))
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    };
+    
+
+    return (
+        <div>
+            <TonConnectButton onClick={handleLogin} />
+        </div>
+    );
+}
+
+export default Login;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
-import { TonConnectButton } from '@tonconnect/ui-react';
+// import { TonConnectButton } from '@tonconnect/ui-react';
+import Login from '../Login/Login'; // Убедитесь, что путь корректен
 
 const Navbar = () => {
     return (
@@ -10,7 +11,7 @@ const Navbar = () => {
             <div className={styles.navbarLinks}>
                 <Link to="/">Home</Link>
             </div>
-            <TonConnectButton />
+            <Login />
         </nav>
     );
 };
