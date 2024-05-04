be5cb9d16e6ea329b4320efd0360d47e3d7413e6
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-04 20:27:08 +0300

    Ваше сообщение о коммите

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,9 +5,30 @@ import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/m
 function ProductList() {
   const navigate = useNavigate(); // Хук для навигации
   const tempProducts = [
-    { id: 1, title: 'Apple MacBook Pro', description: '16-inch, 16GB RAM', image: 'path/to/image1.jpg' },
-    { id: 2, title: 'Apple iPad Air', description: '10.9-inch, 256GB, Space Gray', image: 'path/to/image2.jpg' },
-    { id: 3, title: 'Sony Headphones', description: 'Noise Cancelling, Wireless', image: 'path/to/image3.jpg' }
+    { 
+      id: 1, 
+      title: 'Apple MacBook Pro', 
+      description: '16-inch, 16GB RAM', 
+      image: 'path/to/image1.jpg',
+      lotInfo: 'Лот #123456, доступно 5 штук',
+      shippingInfo: 'Доставка в течение 3-5 рабочих дней'
+    },
+    { 
+      id: 2, 
+      title: 'Apple iPad Air', 
+      description: '10.9-inch, 256GB, Space Gray', 
+      image: 'path/to/image2.jpg',
+      lotInfo: 'Лот #234567, доступно 3 штуки',
+      shippingInfo: 'Бесплатная доставка'
+    },
+    { 
+      id: 3, 
+      title: 'Sony Headphones', 
+      description: 'Noise Cancelling, Wireless', 
+      image: 'path/to/image3.jpg',
+      lotInfo: 'Лот #345678, доступно 10 штук',
+      shippingInfo: 'Доставка за 1 день, платно'
+    }
   ];
 
   const handleCardClick = (id) => {
