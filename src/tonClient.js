6b66a64350e74acf56ccd32f9d8687ed1945d45b
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-08 01:08:47 +0300

    Ваше сообщение о коммите

--- src/tonClient.js
+++ src/tonClient.js
@@ -2,7 +2,7 @@ import { TonClient } from 'ton-web-js';
 
 // Настроить клиента с вашим конечным URL для работы с блокчейном
 const client = new TonClient({
-    endpoint: 'https://your-endpoint',
+    endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
 });
 
 export default client;
