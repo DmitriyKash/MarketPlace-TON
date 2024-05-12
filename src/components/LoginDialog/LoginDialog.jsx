7438007a9ed8031327d4bae22af73ff24cce8bfb
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-12 12:40:18 +0300

    Ваше сообщение о коммите

--- src/components/LoginDialog/LoginDialog.jsx
+++ src/components/LoginDialog/LoginDialog.jsx
@@ -7,12 +7,14 @@ import Button from '@mui/material/Button';
 
 function LoginDialog({ open, onClose, onLogin }) {
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Вход в систему</DialogTitle>
+    <Dialog open={open} onClose={onClose} PaperProps={{
+        style: { backgroundColor: '#ffcccc' } // Легкий красный фон для всего диалога
+      }}>
+      <DialogTitle style={{ color: 'white', backgroundColor: 'red' }}>Вход в систему</DialogTitle>
       <DialogContent>Пожалуйста, войдите через "Connect Wallet", чтобы продолжить покупку.</DialogContent>
       <DialogActions>
-        <Button onClick={onClose}>Отмена</Button>
-        {/* <Button onClick={onLogin} color="primary">Войти</Button> */}
+        <Button onClick={onClose} style={{ color: 'red' }}>Отмена</Button>
+        {/* <Button onClick={onLogin} color="primary" style={{ color: 'white' }}>Войти</Button> */}
       </DialogActions>
     </Dialog>
   );
