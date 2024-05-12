afee257b20b337f0c7293aa9ab32f907d1b1ed75
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-12 12:59:53 +0300

    Ваше сообщение о коммите

--- src/components/LoginDialog/LoginDialog.jsx
+++ src/components/LoginDialog/LoginDialog.jsx
@@ -4,26 +4,20 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { TonConnectButton } from '@tonconnect/ui-react'; // Импорт кнопки TON Connect
+import CardMedia from '@mui/material/CardMedia'; // Импорт для отображения медиаконтента
 
-function LoginDialog({ open, onClose, onAuthentication }) {
-  const handleAuthentication = (isLoggedIn, userData) => {
-    onAuthentication(isLoggedIn, userData);
-    onClose(); // Закрыть диалог после аутентификации
-  };
-
+function LoginDialog({ open, onClose }) {
   return (
     <Dialog open={open} onClose={onClose} aria-labelledby="connect-wallet-dialog-title">
       <DialogTitle id="connect-wallet-dialog-title">Пожалуйста, подключите кошелек</DialogTitle>
       <DialogContent dividers>
-        <Typography gutterBottom>
-          Ваш кошелек не подключен. Чтобы совершать покупки, необходимо его подключить.
-        </Typography>
-        <TonConnectButton 
-          onLogin={(userData) => handleAuthentication(true, userData)}
-          onLogout={() => handleAuthentication(false, null)}
+        <CardMedia
+          component="img"
+          alt="Инструкция по подключению кошелька"
+          height="auto"
+          image="path/to/your-wallet-connect-instruction.gif"
         />
+        <p>Следуйте инструкциям на гифке, чтобы подключить ваш кошелек.</p>
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} color="primary">Закрыть</Button>
