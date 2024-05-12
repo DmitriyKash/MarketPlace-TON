553762c90ced525a1ff80ec0ebddd1cb5143043c
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-12 12:45:21 +0300

    Ваше сообщение о коммите

--- src/components/LoginDialog/LoginDialog.jsx
+++ src/components/LoginDialog/LoginDialog.jsx
@@ -4,17 +4,22 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-function LoginDialog({ open, onClose, onLogin }) {
+function LoginDialog({ open, onClose, onConnect }) {
   return (
-    <Dialog open={open} onClose={onClose} PaperProps={{
-        style: { backgroundColor: '#ffcccc' } // Легкий красный фон для всего диалога
-      }}>
-      <DialogTitle style={{ color: 'white', backgroundColor: 'red' }}>Вход в систему</DialogTitle>
-      <DialogContent>Пожалуйста, войдите через "Connect Wallet", чтобы продолжить покупку.</DialogContent>
+    <Dialog open={open} onClose={onClose} aria-labelledby="connect-wallet-dialog-title">
+      <DialogTitle id="connect-wallet-dialog-title">Пожалуйста, подключите кошелек</DialogTitle>
+      <DialogContent dividers>
+        <Typography gutterBottom>
+          Ваш кошелек не подключен. Чтобы совершать покупки, необходимо его подключить.
+        </Typography>
+        <Button variant="contained" color="secondary" onClick={onConnect} style={{ marginTop: 20 }}>
+          Подключить кошелек
+        </Button>
+      </DialogContent>
       <DialogActions>
-        <Button onClick={onClose} style={{ color: 'red' }}>Отмена</Button>
-        {/* <Button onClick={onLogin} color="primary" style={{ color: 'white' }}>Войти</Button> */}
+        <Button onClick={onClose} color="primary">Закрыть</Button>
       </DialogActions>
     </Dialog>
   );
