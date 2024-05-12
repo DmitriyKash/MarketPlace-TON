cacedf8c4904c22a6c528890ea349b7b32562b78
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-12 14:04:13 +0300

    Ваше сообщение о коммите

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import { Card, CardActionArea, CardContent, CardMedia, Typography, TextField } from '@mui/material';
 import { useProducts } from '../ProductContext/ProductContext';
 
-
 function ProductList() {
   const navigate = useNavigate(); // Хук для навигации
   const [searchTerm, setSearchTerm] = useState('');
@@ -32,20 +31,24 @@ function ProductList() {
         style={{ marginBottom: 20, width: '100%' }}
       />
       {filteredProducts.map((product) => (
-        <Card key={product.id} style={{ margin: 10, width: 'calc(50% - 20px)' }} onClick={() => handleCardClick(product.id)}>
-          <CardActionArea>
+        <Card 
+          key={product.id} 
+          style={{ margin: 10, width: 'calc(50% - 20px)', height: 400, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }} 
+          onClick={() => handleCardClick(product.id)}
+        >
+          <CardActionArea style={{ flexGrow: 1 }}>
             <CardMedia
               component="img"
               alt={product.title}
-              height="140"
+              style={{ height: 140, objectFit: 'cover' }}
               image={product.image}
               title={product.title}
             />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
+              <Typography gutterBottom variant="h5" component="h2" style={{ overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: 1, WebkitBoxOrient: 'vertical' }}>
                 {product.title}
               </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
+              <Typography variant="body2" color="textSecondary" component="p" style={{ overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical' }}>
                 {product.description}
               </Typography>
             </CardContent>
