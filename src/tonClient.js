470de45e22cd63a473f2c393e2277f32e6257b1b
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-15 22:15:26 +0300

    Ваше сообщение о коммите

--- src/tonClient.js
+++ src/tonClient.js
@@ -1,18 +1,42 @@
 /* eslint-disable react-hooks/rules-of-hooks */
+// import { TonClient } from '@eversdk/core';
+// import { libWeb, libWebSetup } from '@eversdk/lib-web';
+
+// // Настройка альтернативного URL для WASM модуля
+// libWebSetup({
+//     binaryURL: '/assets/eversdk.wasm', // Путь к WASM файлу
+// });
+
+// TonClient.useBinaryLibrary(libWeb);
+
+// const client = new TonClient({
+//     network: {
+//         endpoints: ['https://test.ton.dev'], // Для тестовой сети
+//     },
+// });
+
+// export default client;
+
+
+
+
+
 import { TonClient } from '@eversdk/core';
 import { libWeb, libWebSetup } from '@eversdk/lib-web';
 
-// Настройка альтернативного URL для WASM модуля
+// Настройка URL для WASM модуля
 libWebSetup({
-    binaryURL: '/assets/eversdk.wasm', // Путь к WASM файлу
+    binaryURL: '/assets/eversdk.wasm',
 });
 
 TonClient.useBinaryLibrary(libWeb);
 
 const client = new TonClient({
-    network: {
-        endpoints: ['https://test.ton.dev'], // Для тестовой сети
-    },
+  network: {
+    endpoints: ['https://mainnet.evercloud.dev/cd7307b544b74dd0abe0330814e51d6c/graphql'], // Замените YOUR_PROJECT_ID на ваш реальный проект ID
+    access_key: '93472d702aec4563b1f395e88686e7e4', // Замените YOUR_ACCESS_KEY на ваш реальный API ключ
+  },
 });
 
 export default client;
+
