034be4c33a66824c99f69d8af55d25da8f0efabb
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-15 17:03:06 +0300

    Ваше сообщение о коммите

--- src/tonClient.js
+++ src/tonClient.js
@@ -4,7 +4,7 @@ import { libWeb, libWebSetup } from '@eversdk/lib-web';
 
 // Настройка альтернативного URL для WASM модуля
 libWebSetup({
-    binaryURL: '/assets/eversdk_1_30_1.wasm', // Путь к WASM файлу
+    binaryURL: '/assets/eversdk.wasm', // Путь к WASM файлу
 });
 
 TonClient.useBinaryLibrary(libWeb);
