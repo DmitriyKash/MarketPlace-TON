452e79ab5115d91fee04ecfc301f2b3931cdb02c
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-21 23:22:19 +0300

    Ваше сообщение о коммите

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import HomePage from './pages/HomePage';
-// import ProductPage from './pages/ProductPage';
-// import Navbar from './components/Navbar/Navbar';
-// import { TonConnectUIProvider } from '@tonconnect/ui-react';
-
-// function App() {
-//     return (
-//         <TonConnectUIProvider manifestUrl="https://harmonious-fenglisu-6d5f55.netlify.app/tonconnect-manifest.json">
-//             <Router>
-//                 <Navbar />
-//                 <Routes>
-//                     <Route path="/" element={<HomePage />} />
-//                     <Route path="/product/:id" element={<ProductPage />} />
-//                 </Routes>
-//             </Router>
-//         </TonConnectUIProvider>
-//     );
-// }
-
-// export default App;
-
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -28,7 +5,6 @@ import HomePage from './pages/HomePage';
 import ProductDetails from'./components/ProductDetails/ProductDetails';
 import TransactionForm from './components/TransactionForm/TransactionForm';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import { CartProvider } from './pages/CartContext';
 import AuthProvider from './components/AuthContext/AuthContext';
 import { ProductProvider } from './components/ProductContext/ProductContext';
 
@@ -51,7 +27,6 @@ function App() {
         <Router>
         <AuthProvider>
           <ProductProvider>
-        <CartProvider> {/* Wrap all routes with CartProvider */}
             <div className="App">
             <Routes>
                 <Route path="/" element={<HomePage />} />
@@ -59,7 +34,6 @@ function App() {
                 <Route path="/transaction" element={<TransactionForm />} />
             </Routes>
             </div>
-        </CartProvider>
         </ProductProvider>
         </AuthProvider>
         </Router>
