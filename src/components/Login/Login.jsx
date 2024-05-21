49d813b2a0d96573be51ea15dd6ec1d2279a1b34
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-21 11:24:50 +0300

    Ваше сообщение о коммите

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -21,7 +21,7 @@ function Login() {
         <TonConnectButton 
           onLogin={(userData) => handleAuthentication(true, userData)}
           onLogout={() => handleAuthentication(false, null)}
-          style={{ backgroundColor: 'blue', color: 'white' }} // Пример изменения фона и цвета текста
+          style={{ backgroundColor: 'blue', color: 'white', width: '20px' }} // Пример изменения фона и цвета текста
         />
       )}
     </div>
